f7f37b4069241bb5b6ef8285d010e5a2a45dedaf
NehaMaradi <neha.22cs102@sode-edu.in>
2026-03-07 20:20:36 +0530

    Add App2 component with two sections

---
+++ App2.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App1">
+      <h1>This is my default app calledApp! div 1</h1>
+      <ul>
+        <li>Milk</li>
+        <li>Bread</li>
+        <li>Eggs</li>
+      </ul>
+
+      <button>Click Me</button>
+    </div>
+    <div className="App2">
+      <h1>This is my default app calledApp! div 2</h1>
+      <ul>
+        <li>Milk</li>
+        <li>Bread</li>
+        <li>Eggs</li>
+      </ul>
+
+      <button>Click Me</button>
+    </div>
+    </>
+  );
+}
+
+export default App;
