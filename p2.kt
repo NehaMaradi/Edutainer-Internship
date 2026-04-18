f17bbd64b0d76bfc3961a9e4d0a40be0ebf767f0
NehaMaradi <neha.22cs102@sode-edu.in>
2026-04-18 13:06:56 +0530

    Implement comparison of two integers with output

---
+++ p2.kt
@@ -0,0 +1,14 @@
+//Relational Operators with conditional statements
+fun main(){
+    val a = -12
+    val b = 12
+    //use of greater than operator
+    val max = if (a > b) //false
+    {
+        println("a s larger than b.")
+    }
+    else
+    {
+        println("b is larger than a.")
+    }
+}
