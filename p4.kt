39645ec958defccab948bdbb7cd02db77d1c1eae
NehaMaradi <neha.22cs102@sode-edu.in>
2026-04-21 18:05:04 +0530

    Add Kotlin program for assignment operators
    Demonstrate assignment operators in Kotlin with examples.

---
+++ p4.kt
@@ -0,0 +1,15 @@
+// Assignment operators
+fun main() {
+    var a = 20
+    var b = 5
+    a+= b   //a=a+b, a= 20+5 = 25
+    print(a);
+    a-=b   //a=a-b, a= 20-5 = 15
+    println(a)
+    a*=b   //a=a*b, a= 20*5 = 100
+    println(a)
+    a/=b
+    println(a)
+    a%=b
+    println(a)
+}
