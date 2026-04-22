f5401caf3fbdd25516088ed39809a5693f0fc09e
NehaMaradi <neha.22cs102@sode-edu.in>
2026-04-22 15:57:01 +0530

    Add main function to compare two numbers

---
+++ p6.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+    var a=5
+    var b=10
+    if(a > b){
+        print("Number 5 is larger than Number 10")
+    }
+    else if(a < b){
+        print("number 10 is larger than number 5")
+    }
