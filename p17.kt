9876fb651e5db4c206f03d47b3159ecdd7d4ae7a
NehaMaradi <neha.22cs102@sode-edu.in>
2026-05-02 13:56:46 +0530

    Add examples of nested and inner classes in Kotlin

--- p17.kt
+++ p17.kt
@@ -1,4 +1,4 @@
-//Kotlin Nested Class
+//Kotlin Nested Class eg1
 fun main() {
     val obj = Outer2.Nested()
     print(obj.foo())
@@ -6,5 +6,17 @@ fun main() {
 class Outer2 {
     class Nested {
         fun foo()="Welcome to Android development"
+    }
+}
+
+//eg 2
+fun main(){
+    val obj = Outer3().Inner3()
+    print(obj.foo())
+}
+class Outer3{
+    private val welcomeMessage: String = "Welcome to Android development"
+    inner class Inner3{
+        fun foo() = welcomeMessage
     }
 }
